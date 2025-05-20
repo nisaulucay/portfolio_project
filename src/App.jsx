76b3b19b531c97fc0fc86fe0f350a64b2573c032
nisaulucay <hayrunnisaulucaya@gmail.com>
2025-05-20 18:55:28 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,18 @@ import React, { useEffect, useState } from 'react';
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
 
-import Header from "../../portfolio_project/src/components/layout/Header";
-import Footer from "../../portfolio_project/src/components/layout/Footer";
+import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
 
-import HeroSection from "../../portfolio_project/src/components/sections/HeroSection";
-import AboutSection from "../../portfolio_project/src/components/sections/AboutSection";
-import SkillsSection from "../../portfolio_project/src/components/sections/SkillsSection";
-import PortfolioSection from "../../portfolio_project/src/components/sections/PortfolioSection";
-import ContactSection from '../../portfolio_project/src/components/sections/ContactSection';
+import HeroSection from "./components/sections/HeroSection";
+import AboutSection from "./components/sections/AboutSection";
+import SkillsSection from "./components/sections/SkillsSection";
+import PortfolioSection from "./components/sections/PortfolioSection";
+import ContactSection from "./components/sections/ContactSection";
 
-import { TooltipProvider } from "../../portfolio_project/src/components/ui/tooltip";
-
+import { TooltipProvider } from "./components/ui/tooltip";
 import { Toaster } from 'sonner';
+
 
 
 function App() {
